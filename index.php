cd962585a63bf57f638093d68c4bf11d6ef026b3
estradaisaac168 <estradaisaac168@gmail.com>
2025-02-20 04:24:47 +0000

    Tercer actualizacion todo funciona bien

--- index.php
+++ index.php
@@ -10,10 +10,10 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header('Content-Type: application/json');
 
 // Manejar solicitudes OPTIONS (Preflight)
-// if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-//     http_response_code(200);
-//     exit();
-// }
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 
 
